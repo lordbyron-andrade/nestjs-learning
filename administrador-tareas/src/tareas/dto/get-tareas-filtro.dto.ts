f8c2de0282a7c4d5386f75221fd9c3e1fc8815bf
Luis Andrade <luis@luis-VirtualBox>
2021-11-09 16:18:54 -0600

    Actualizar get-tareas-filtro.dto.ts

--- administrador-tareas/src/tareas/dto/get-tareas-filtro.dto.ts
+++ administrador-tareas/src/tareas/dto/get-tareas-filtro.dto.ts
@@ -1,7 +1,21 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { StatusTarea } from '../tareas.model';
 
 export class GetTareasFiltroDto {
-  /*El signo de interrogación significa que el parametro es opcional puede o no ser incluiído */
+  /*El signo de interrogación significa que el parametro es opcional puede o no ser incluiído 
+  
   status?: StatusTarea;
+  search?: string;
+
+  */
+
+  /*El segundo cambio que se hizo  a este DTO es el de agrear decoraciones para demostrar que es opcional y que se pueda validar
+  dado que este ya es un DTO y que usamos estos para las validaciones anteriores basta con agregar las decoraciones correspondientes*/
+  @IsOptional()
+  @IsEnum(StatusTarea)
+  status?: StatusTarea;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
