29b0a9d654d203eef10d066fc9ccf9e5ce59bc4c
Luis Andrade <luis@Mint-A01>
2022-03-30 21:51:21 -0600

    Actualizar package-lock.json, package.json y 3 más archivos...

--- nestjs-fundamentals/iluvcoffee/src/app.module.ts
+++ nestjs-fundamentals/iluvcoffee/src/app.module.ts
@@ -5,9 +5,22 @@ import { CoffesController } from './coffes/coffes.controller';
 import { CoffeServiceService } from './coffe-service/coffe-service.service';
 import { CoffeService } from './coffe/coffe.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'ljac1978',
+      database: 'nestjsfundamentals',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjs-fundamentals/iluvcoffee/src/coffees/coffees.module.ts
+++ nestjs-fundamentals/iluvcoffee/src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeService } from 'src/coffe/coffe.service';
 import { CoffesController } from 'src/coffes/coffes.controller';
+import { Coffe } from 'src/coffes/entities/coffe.entity';
 
-@Module({ controllers: [CoffesController], providers: [CoffeService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffe])],
+  controllers: [CoffesController],
+  providers: [CoffeService],
+})
 export class CoffeesModule {}
